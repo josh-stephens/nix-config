fa91c6d16ba9108e7893da797c2d96d19a0134ee
Josh Symonds <veraticus@gmail.com>
2023-04-27 19:27:43 -0700

    Fix nvidia configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,6 +85,9 @@
     };
   };
 
+  # Set up nvidia
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,5 +1,4 @@
 {
-  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
 
 # Optionally, you may need to select the appropriate driver version for your specific GPU.
