40d2677fe471dffeed531c33e9624f7b6e21b72f
Josh Symonds <veraticus@gmail.com>
2023-04-29 16:11:24 -0700

    get rid of xdg for this

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -69,10 +69,6 @@
     source = ./zsh/zsh;
     recursive = true;
   };
-  xdg.configFile."nvim" = {
-    source = ./nvim;
-    recursive = true;
-  };
 
   home.file.".zshrc".source = ./zsh/.zshrc;
   home.file.".zshenv".source = ./zsh/.zshenv;
