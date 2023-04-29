1eed978a50a56d586324d69032428de021ceda94
Josh Symonds <veraticus@gmail.com>
2023-04-29 16:35:44 -0700

    More nvim pruning

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -13,8 +13,32 @@
     enable = true;
     colorscheme = "catppuccin";
 
+    lsp = {
+      enable = true;
+      bashls.enable = true;
+      html.enable = true;
+      jsonls.enable = true;
+      nix-lsp.enable = true;
+      gopls.enable = true;
+      rust-analyzer.enable = true;
+    };
+    treesitter = {
+      enable = true;
+      indent = true;
+    };
+    mini = {
+      enable = true;
+      ai.enable = true;
+      jump.enable = true;
+    };
+    gitsigns = {
+      enable = true;
+    };
+
     extraPlugins = [
       pkgs.vimExtraPlugins.catppuccin
+      pkgs.vimExtraPlugins.leap
+      pkgs.vimExtraPlugins.nvim-web-devicons
     ];
   };
 }
