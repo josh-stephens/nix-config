29cec8b025996a75f228fe57ae25d29f30a7c31d
Josh Symonds <josh@joshsymonds.com>
2023-05-01 15:55:17 -0700

    Try breaking out config

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -1,6 +1,8 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [
     inputs.nixneovim.nixosModules.default
+
+    ./lsp.nix
   ];
 
   nixpkgs = {
@@ -14,30 +16,6 @@
     colorscheme = "catppuccin-mocha";
 
     plugins = {
-      lsp = {
-        enable = true;
-        servers = {
-          bashls.enable = true;
-          html.enable = true;
-          jsonls.enable = true;
-          rnix-lsp.enable = true;
-          gopls.enable = true;
-          rust-analyzer.enable = true;
-        };
-      };
-      lspkind = {
-        enable = true;
-        extraLua.post = ''
-          local lspkind = require("lspkind")
-          lspkind.init({
-            symbol_map = {
-              Copilot = "",
-            },
-          })
-
-          vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#6CC644"})
-        '';
-      };
       treesitter = {
         enable = true;
         indent = true;
---
+++ home-manager/nvim/lsp.nix
@@ -0,0 +1,40 @@
+{ inputs, lib, config, pkgs, ... }: {
+  imports = [
+    inputs.nixneovim.nixosModules.default
+  ];
+
+  nixpkgs = {
+    overlays = [
+      inputs.nixneovim.overlays.default
+    ];
+  };
+
+  programs.nixneovim = {
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          bashls.enable = true;
+          html.enable = true;
+          jsonls.enable = true;
+          rnix-lsp.enable = true;
+          gopls.enable = true;
+          rust-analyzer.enable = true;
+        };
+      };
+      lspkind = {
+        enable = true;
+        extraLua.post = ''
+          local lspkind = require("lspkind")
+          lspkind.init({
+            symbol_map = {
+              Copilot = "",
+            },
+          })
+
+          vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#6CC644"})
+        '';
+      };
+    };
+  };
+}
