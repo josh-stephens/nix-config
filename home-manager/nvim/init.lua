f01a15dad27963e243f0f729b4499f4aa43e5adb
Josh Symonds <josh@joshsymonds.com>
2023-05-01 16:10:34 -0700

    Make telescope open the file in nvim-tree

--- home-manager/nvim/init.lua
+++ home-manager/nvim/init.lua
@@ -120,6 +120,30 @@ require("cmp").setup({
 end
 })
 
+local transform_mod = require('telescope.actions.mt').transform_mod
+local actions = require('telescope.actions')
+local mod = {}
+mod.open_in_nvim_tree = function(prompt_bufnr)
+    local cur_win = vim.api.nvim_get_current_win()
+    vim.cmd("NvimTreeFindFile")
+    vim.api.nvim_set_current_win(cur_win)
+end
+
+mod = transform_mod(mod)
+
+require("telescope").setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<CR>"] = actions.select_default + mod.open_in_nvim_tree,
+            },
+            n = {
+                ["<CR>"] = actions.select_default + mod.open_in_nvim_tree,
+            },
+        },
+    },
+}
+
 -- LSP movement
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
