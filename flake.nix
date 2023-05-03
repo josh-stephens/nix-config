89260c09a7292c6a789938450f02daa5ddb5f403
Josh Symonds <veraticus@gmail.com>
2023-05-03 14:37:50 -0700

    Set upstream nix

--- flake.nix
+++ flake.nix
@@ -7,9 +7,10 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Darwin
-    nixpkgsDarwin.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
     darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgsDarwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    darwin-nix.url = "github:yorickvP/nix/boost-regex";
 
     # Secrets
     agenix.url = "github:ryantm/agenix";
--- nixos/cloudbank/default.nix
+++ nixos/cloudbank/default.nix
@@ -39,6 +39,8 @@ in { inputs, lib, config, pkgs, ... }: {
 
 
   nix = {
+    package = inputs.darwin-nix.packages.${system}.nix;
+
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
