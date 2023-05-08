98f1751aeec621640a4cb41135363c197bdac769
Josh Symonds <josh@joshsymonds.com>
2023-05-08 11:50:55 -0700

    Many OSX improvements

--- home-manager/sketchybar/sketchybar/plugins/net.sh
+++ home-manager/sketchybar/sketchybar/plugins/net.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-LABEL=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'NR==13 {print $2}')
+LABEL=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk 'NR==13 {first = $1; $1=""; print $0}' | sed 's/^ //g')
 
-sketchybar --set $NAME label=$LABEL
+sketchybar --set "${NAME}" label="${LABEL}"
