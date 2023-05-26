b715c2499631a7809b727a8bec5da131d5eb6a79
Josh Symonds <josh@joshsymonds.com>
2023-05-25 21:10:06 -0700

    fix locktimes

--- home-manager/hyprland/hypr/scripts/sleep.sh
+++ home-manager/hyprland/hypr/scripts/sleep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 swayidle -w \
         before-sleep "$HOME/.config/hypr/scripts/lock.sh" \
-        timeout 160 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
+        timeout 300 'temp=$(brightnessctl g); brightnessctl set $((temp / 2))' \
             resume 'temp=$(brightnessctl g); brightnessctl set $((temp * 2))' \
-        timeout 300 "$HOME/.config/hypr/scripts/lock.sh & sleep 0.1 && hyprctl dispatch dpms off" \
+        timeout 1800 "$HOME/.config/hypr/scripts/lock.sh & sleep 0.1 && hyprctl dispatch dpms off" \
             resume 'hyprctl dispatch dpms on'
