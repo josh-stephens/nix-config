84b0edf0341f8cd24212ec0b36c49035b5f81862
Josh Symonds <josh@joshsymonds.com>
2023-07-17 17:10:59 -0700

    Add winedlloverrides

--- home-manager/xiv/xiv/start.sh
+++ home-manager/xiv/xiv/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export XL_SECRET_PROVIDER=FILE
+export WINEDLLOVERRIDES="d3dcompiler_47=n;dxgi=n,b"
 
 rm -f "${HOME}/.xlcore/wineprefix/drive_c/users/$(whoami)/AppData/Local/NUCefSharp/pid.txt"
 gamemoderun XIVLauncher.Core
