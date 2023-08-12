72c476f96a016c430284a0565d9c34abea947f81
Josh Symonds <josh@joshsymonds.com>
2023-08-12 08:13:42 -0700

    add swaync configs

--- home-manager/swaync/default.nix
+++ home-manager/swaync/default.nix
@@ -1,6 +1,30 @@
-{ inputs, lib, config, pkgs, ... }: {
+{ inputs, lib, config, pkgs, ... }:
+{
   home.packages = [
     pkgs.unstable.swaynotificationcenter
   ];
 
+  xdg.configFile."swaync" = {
+    source = ./swaync;
+    recursive = true;
+  };
+
+  systemd.user.services.swaync = {
+    Unit = {
+      Description = "swaynotificationcenter";
+      PartOf = "graphical-session.target";
+      After = "graphical-session.target";
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+    };
+    Service = {
+      Type = "dbus";
+      ExecStart = "${pkgs.swaync}/bin/swaync";
+      ExecReload = "${pkgs.swaync}/bin/swaync-client --reload-config ; ${pkgs.swaync}/bin/swaync-client --reload-css";
+      Restart = "on-failure";
+
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
