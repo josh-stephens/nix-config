5d6fbf6cd44701e593b819ede3c8a32ccfb3cd37
Josh Symonds <josh@joshsymonds.com>
2023-08-21 17:33:02 -0700

    Initial eww pass

--- home-manager/eww/default.nix
+++ home-manager/eww/default.nix
@@ -1,4 +1,10 @@
 { inputs, lib, config, pkgs, ... }: {
+  home.packages = with pkgs.unstable; [
+    coreutils-full
+    jq
+    socat
+  ];
+
   programs.eww = {
     enable = true;
     package = pkgs.unstable.eww-wayland;
