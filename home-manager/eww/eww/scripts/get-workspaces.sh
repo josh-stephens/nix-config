0e6c5fdefd68a05edea5474b65d87a8bf823f4c9
Josh Symonds <josh@joshsymonds.com>
2023-09-15 18:56:53 -0700

    Improve spaces()

--- home-manager/eww/eww/scripts/get-workspaces.sh
+++ home-manager/eww/eww/scripts/get-workspaces.sh
@@ -11,8 +11,15 @@ WORKSPACE_ICONS=$(jq -n '{
 }')
 
 spaces (){
-	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
-	seq 1 10 | jq --argjson icons "${WORKSPACE_ICONS}" --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0), icon: ($icons[.]//null)}) | map(select(.windows != 0))'
+  WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
+  ACTIVE_WORKSPACE_DATA=$(hyprctl activeworkspace -j)
+  ACTIVE_WORKSPACE_ID=$(echo "${ACTIVE_WORKSPACE_DATA}" | jq -r '.id | tostring')
+  ACTIVE_WORKSPACE_WINDOWS=$(echo "${ACTIVE_WORKSPACE_DATA}" | jq --argjson icons "${WORKSPACE_ICONS}" '{id: .id | tostring, windows: .windows, icon: ($icons[.id | tostring]//null)}')
+  WORKSPACE_JSON=$(seq 1 10 | jq --argjson icons "${WORKSPACE_ICONS}" --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0), icon: ($icons[.]//null)}) | map(select(.windows != 0))')
+  FINAL_JSON=$(echo "${WORKSPACE_JSON}" | jq -Mc --argjson active "${ACTIVE_WORKSPACE_WINDOWS}" '. + [$active] | unique_by(.id) | sort_by(.id)')
+
+
+  echo "${FINAL_JSON}"
 }
 
 spaces
