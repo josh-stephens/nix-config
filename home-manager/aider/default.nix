5cf7d3bad8f3ef83e843bc942f4a3289cd3c005c
Josh Symonds <josh@joshsymonds.com>
2025-02-24 21:41:20 -0800

    feat: add aider configuration for home-manager

---
+++ home-manager/aider/default.nix
@@ -0,0 +1,15 @@
+{ inputs, lib, config, pkgs, ... }: {
+  home.packages = [ pkgs.aider ];
+
+  home.file.".aider.model.metadata.json" = {
+    source = ./aider/.aider.model.metadata.json;
+  };
+
+  home.file.".aider.model.settings.yml" = {
+    source = ./aider/.aider.model.settings.yml;
+  };
+
+  home.file.".aider.conf.yml" = {
+    source = ./aider/.aider.conf.yml;
+  };
+}
