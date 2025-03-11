b5b12155145b2967afb94373069578823d8a8a99
Josh Symonds <josh@joshsymonds.com>
2025-03-11 12:53:16 -0700

    feat: Update nixpkgs-unstable input to specific kitty version branch

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/leiserfg/nixpkgs:kitty-0.40.0";
     nur.url = "github:nix-community/NUR";
 
     # Darwin
