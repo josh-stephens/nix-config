c7fd43131e328d198c561c2936cc74c607eed6f3
Josh Symonds <josh@joshsymonds.com>
2025-06-04 21:42:40 -0700

    linkpearl?

--- home-manager/aarch64-darwin.nix
+++ home-manager/aarch64-darwin.nix
@@ -5,10 +5,19 @@
     ./devspaces-client
     ./ssh-hosts
     ./ssh-config
+    inputs.linkpearl.homeManagerModules.default
   ];
 
   home.homeDirectory = "/Users/joshsymonds";
   
+  # Linkpearl client configuration
+  services.linkpearl = {
+    enable = true;
+    secretFile = "${config.xdg.configHome}/linkpearl/secret";
+    join = [ "ultraviolet:9437" ];  # Connect to ultraviolet on Tailnet
+    nodeId = "cloudbank";
+    verbose = false;
+  };
 
   programs.zsh.shellAliases.update = "sudo darwin-rebuild switch --flake \".#$(hostname -s)\"";
   programs.kitty.font.size = 13;
